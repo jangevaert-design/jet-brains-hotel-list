cda9b55ca4c319377ebf9eafabe1aacbff65ece1
Jan Gevaert <jan.gevaert@gmail.com>
2020-11-05 09:26:31 -0700

    Initial commit

---
+++ Problems/Guest list of a hotel/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        String jane = scanner.next();
+        String kate = scanner.next();
+        String john = scanner.next();
+        String mary = scanner.next();
+        String susan = scanner.next();
+        String paul = scanner.next();
+        String boris = scanner.next();
+        String ann = scanner.next();
+        
+        System.out.println(ann);
+        System.out.println(boris);
+        System.out.println(paul);
+        System.out.println(susan);
+        System.out.println(mary);
+        System.out.println(john);
+        System.out.println(kate);
+        System.out.println(jane);
+    }
+}
